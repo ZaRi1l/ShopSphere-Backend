db69f2c1f8042bcde812b7e92443bb9a57fdc90a
ZaRi1l <zari_09@naver.com>
2025-05-08 16:37:21 +0900

    delete trash

--- src/main/java/com/shopsphere/shopsphere_web/MemoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shopsphere.shopsphere_web;
-
-import com.shopsphere.shopsphere_web.dto.Memo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemoRepository extends JpaRepository<Memo, Long> {
-}
