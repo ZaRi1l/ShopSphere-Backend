632652dbaede5c07712836218f494bd9b43c9d05
kimdonghyun296 <2q3w4e5r6t7y@naver.com>
2025-06-27 13:57:56 +0900

    kim push test
    kim push test

--- src/main/java/com/shopsphere/shopsphere_web/ShopsphereWebApplication.java
+++ src/main/java/com/shopsphere/shopsphere_web/ShopsphereWebApplication.java
@@ -4,6 +4,7 @@ import com.shopsphere.shopsphere_web.config.KakaoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication
